fc568f856e5133c7d4eb217b0ee7de4282140739
dangthang1998 <phamdangthang98@gmail.com>
2021-05-17 23:07:02 +0700

    update

--- app/Modules/Web/Resources/Views/post/new.blade.php
+++ app/Modules/Web/Resources/Views/post/new.blade.php
@@ -148,30 +148,5 @@
 		</div>
 	</div>
 
-	<div class="register">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6 col-md-12 col-sm-12 col-12 qc4">
-					<a href=""><img src="{{ asset('images/dky1.jpg') }}" alt=""></a>
-				</div>
-				<div class="col-lg-6 col-md-12 col-sm-12 col-12 form-register">
-					<h2>ĐĂNG KÝ NHẬN DỰ ÁN HOT</h2>
-					<p>* Để nhận đượcie nều ưu đãi nhất bạn vui lòng đăng ký thông tin phía dưới :</p>
-					<form action="#">
-						<div class="row">
-							<div class="col-lg-6">
-								<input type="text" placeholder="Họ và tên">
-								<input type="email" placeholder="Email">
-								<input type="text" placeholder="Số điện thoại">
-							</div>
-							<div class="col-lg-6">
-								<textarea name="" id="" rows="3" placeholder="Nội dung"></textarea>
-								<button class="register-now">ĐĂNG KÝ NGAY</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+	@include('web::includes.form-register')
 @endsection
